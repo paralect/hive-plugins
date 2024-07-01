b838c0bd973a3dd93768fddc5087db6821dded22
Igor Krasnik <ihar.krasnik@gmail.com>
2024-07-01 20:14:19 +0200

    Initial auth package with tokens only

---
+++ auth/resources/tokens/tokens.schema.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+module.exports = Joi.object({
+  _id: Joi.string(),
+  createdOn: Joi.date(),
+  updatedOn: Joi.date(),
+  user: Joi.object({
+    _id: Joi.string(),
+  }).required(),
+  token: Joi.string().required(),
+  otp: Joi.string().allow(null).allow(''),
+  page: Joi.object({
+    _id: Joi.string(),
+  }).allow(null),
+});
